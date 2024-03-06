97ac49bbbe5a3053ce93e13a317bdddf53396e84
Adam G <147444558+usradam@users.noreply.github.com>
2024-03-06 07:19:21 -1000

    Add files via upload

---
+++ template.c
@@ -0,0 +1,63 @@
+/*****************************************************************
+//
+//  NAME:        
+//
+//  HOMEWORK:    
+//
+//  CLASS:       ICS 212
+//
+//  INSTRUCTOR:  
+//
+//  DATE:        September 3, 2112
+//
+//  FILE:        template.c
+//
+//  DESCRIPTION:
+//   Describe the file
+//
+****************************************************************/
+
+*** include necessary header files for this source file ***
+
+*** if you need, you can have function prototypes here ***
+
+/*****************************************************************
+//
+//  Function name: main
+//
+//  DESCRIPTION:   Don't forget to describe what your main
+//                 function does.
+//
+//  Parameters:    argc (int) : The number of elements in argv
+//                 argv (char*[]) : An array of arguments passed
+//                                  to the program.
+//
+//  Return values:  0 : some meaning
+//                 -1 : some meaning
+//
+****************************************************************/
+
+int main(int argc, char* argv[])
+{
+    ...
+}
+
+/*****************************************************************
+//
+//  Function name: foo
+//
+//  DESCRIPTION:   A template function
+//                 This function does not do anything.
+//                 Please describe your function correctly.
+//
+//  Parameters:    bar (int) : Describe the meaning
+//
+//  Return values:  0 : some meaning
+//                 -1 : some meaning
+//
+****************************************************************/
+
+int foo(int bar)
+{
+    ...
+}
